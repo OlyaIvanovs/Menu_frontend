9fce8ef82463777bc03b2c2b4f5083edfc35347e
Olya Ivanova <olya@ivanovs.info>
2017-11-26 06:54:04 +1300

    Adding a new category.

--- menu/src/components/addCategory.vue
+++ menu/src/components/addCategory.vue
@@ -10,7 +10,7 @@
       <div class="field">
         <input type="text" placeholder="New category" class="input" v-model='category'>
       </div>
-      <button type="submit" class="button is-info">Add</button>
+      <button type="submit" class="button is-info" v-on:click.prevent="post">Add</button>
       <div>{{ category }}</div>
     </form>
   </div>
@@ -26,7 +26,16 @@ export default {
     }
   },
   methods: {
-
+    post: function() {
+      this.$http.post("http://localhost:8000/api/addcategory", {
+        name: this.category
+      }).then(function(data){
+        this.$http.get('http://127.0.0.1:8000/api/categorieslist').then(function(data) {
+            this.categories = data.body;
+        })
+        this.category = '';
+      });
+    }
   },
   created() {
     this.$http.get('http://127.0.0.1:8000/api/categorieslist').then(function(data) {
--- menu/src/components/addRecipe.vue
+++ menu/src/components/addRecipe.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div style="width: 60%;">
     <h2 class="title is-4">Add a new recipe</h2>
     <form>
       <div class="field">
         <input type="text" placeholder="Title" class="input">
       </div>
       <div class="field">
-        <textarea class="textarea is-info" type="text" placeholder="Info textarea"></textarea>
+        <textarea class="textarea is-info" rows='10' type="text" placeholder="Method"></textarea>
       </div>
       <div class="field">
         <div class="select">
@@ -20,6 +20,16 @@
         <i class="fa fa-plus-circle" aria-hidden="true"></i> Add a new category
       </router-link>
       <br><br>
+      <div class="field">
+        <input class="input" type="text" list="categoryList" v-on:change="onChange" v-model="selectedCategory">
+        <datalist id="categoryList">
+          <option v-for="category in categories">{{ category.name }}</option>
+        </datalist>
+      </div>  
+      <div class="notification is-info" v-if="addedNewCategory">
+        <button class="delete"></button>
+        A new category: <b>{{ selectedCategory }}</b> was added.
+      </div>
       <div class="field">
         <button type="submit" class="button is-info">Add a recipe</button>
       </div>
@@ -38,6 +48,18 @@ export default {
         method: '',
         category: ''
       },
+      selectedCategory: '',
+      addedNewCategory: false
+    }
+  },
+  methods: {
+    onChange: function() {
+      console.log(this.selectedCategory);
+      this.$http.post("http://localhost:8000/api/addcategory", {
+        name: this.selectedCategory
+      }).then(function(data){
+        this.addedNewCategory = true;
+      });
     }
   },
   created() {
